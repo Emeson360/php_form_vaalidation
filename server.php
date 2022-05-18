7615aac8fd80d918bd4ee167411d9bf96c4ddd36
Emeh Tochukwu Emmanuel <emehemmanuel1@gmail.com>
2022-05-18 16:19:22 +0100

    validated reg and login form

--- server.php
+++ server.php
@@ -4,8 +4,6 @@
 session_start();
 
 // Declaring and hosting the variables
-$username = "";
-$email = "";
 $errors = array();
 $_SESSION['success'] = "";
 
@@ -49,6 +47,11 @@ if (isset($_POST['reg_user'])) {
     if (empty($email)) {
         array_push($errors, "Email is required");
     }
+    else {
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            array_push($errors, "Invalid email format");
+        }
+    }
     if (empty($password)) {
         array_push($errors, "Password is required");
     }
@@ -75,15 +78,68 @@ if (isset($_POST['reg_user'])) {
         }
         
     }
-    
-     
-    
-    
 
+    // if the form is error free
+    if (count($errors) == 0) {
+        //Inserting data into the table
+        $sql = "INSERT INTO users (name, username, email, password, cpassword) VALUES ('$name', '$username', '$email', '$password', '$cpassword')";
 
+        if (mysqli_query($con, $sql)) {
+            // echo "<br>";
+            // echo "New record added successfully";
+            // go to login page
+            header('location: login.php');
+        }
+        else {
+            echo "error: " . mysqli_error($con);
+        }
+
+        
+    }
 
 }
 
+//  User login
+if (isset($_POST['login_user'])) {
+    $username = mysqli_real_escape_string($con, $_POST['username']);
+    $password = mysqli_real_escape_string($con, $_POST['password']);
+
+
+    // Error for empty input field
+    if (empty($username)) {
+        array_push($errors, "Username is required");
+    }
+    if (empty($password)) {
+        array_push($errors, "Password is required");
+    }
+
+    // if form is free from errors
+    if (count($errors) == 0) {
+        // Read from the Database
+        $sql = "SELECT * FROM users WHERE username = '$username' AND password = '$password'";
+
+        $result = mysqli_query($con, $sql);
+
+        // check if user data is in the Database
+        if (mysqli_num_rows($result) == 1) {
+            // store username in the session variable
+            $_SESSION['username'] = $username;
+
+            // Message
+            $_SESSION['success'] = "You have logged in successfully!!!";
+
+            // Redirect to index
+            header('location: index.php');
+        }
+        else {
+            // Username or password not matched
+            array_push($errors, "incorrect username or password");
+        }
+
+    }
+
+    
+}
 
 
 ?>
